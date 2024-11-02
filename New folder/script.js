63c7923bee335e7c3958cc3deba3074ea35af44c
Honeyliz <123honeyliza@gmail.com>
2024-11-02 13:24:45 +0100

    Add files via upload

---
+++ New folder/script.js
@@ -0,0 +1,5 @@
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for registering!');
+    // Additional registration logic can go here
+});
